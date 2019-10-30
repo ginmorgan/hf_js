9cb5777106fd1e7d5cea5f13c240ba50091b5901
Pavel <49414734+ginmorgan@users.noreply.github.com>
2019-10-31 00:13:51 +0400

    Array

--- 03 array/js/array.js
+++ 03 array/js/array.js
@@ -13,14 +13,27 @@
 //var last = products.length - 1;
 //var recent = products[last];
 //document.write("Last ice flavour is: " + recent + "<br>" + "<br>");
-
+//--------------------------------------------------------------------
 var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 55, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
-var last = scores.length - 1;
+//var last = scores.length - 1;
 var i = 0;
-var count = 0;
-while (i <= last) {
+var value;
+var maxValue = 0;
+while (i < scores.length) {
     document.write("Раствор №" + i + " " + "очков: " + scores[i] + "<br>");
-    i = i + 1;
-    count = count + 1;
+    value = scores[i];
+    if (maxValue < value) {
+        maxValue = value
+    }
+    i++; // i = i + 1
 }
-document.write("Всего тестов: " + count + "<br>");
+document.write("<br> Всего тестов: " + scores.length + "<br>");
+document.write("Максимум очков: " + maxValue + "<br>");
+var array = [];
+for (i = 0; i < scores.length; i++) {
+    if (maxValue == scores[i]) {
+        array.push(i + " ");
+    }
+}
+document.write("Победили образцы №: " + array);
+//--------------------------------------------------------------------
